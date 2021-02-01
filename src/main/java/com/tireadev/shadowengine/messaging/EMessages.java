b8d9c2f8c805a7789231891f65a919e5342ff636
TheTirea <30987354+TheTirea@users.noreply.github.com>
2021-02-01 11:27:52 +0100

    Message Bus

--- src/main/java/com/tireadev/shadowengine/messaging/EMessages.java
+++ src/main/java/com/tireadev/shadowengine/messaging/EMessages.java
@@ -1,5 +1,7 @@
 package com.tireadev.shadowengine.messaging;
 
 public enum EMessages {
-
+    NULL,
+    UPDATE_KEY_EVENT,
+    UPDATE_MOUSE_EVENT,
 }
--- src/main/java/com/tireadev/shadowengine/messaging/MessageBus.java
+++ src/main/java/com/tireadev/shadowengine/messaging/MessageBus.java
@@ -1,5 +1,30 @@
 package com.tireadev.shadowengine.messaging;
 
+import java.util.ArrayList;
+
 public class MessageBus {
+
+    private static final ArrayList<EMessages> messagesBuffer = new ArrayList<>();
+
+    public static boolean dispatchMessage(EMessages eMessage) {
+        return messagesBuffer.add(eMessage);
+    }
+
+    public static boolean getMessage(EMessages eMessage) {
+        for (EMessages eMessageFor : messagesBuffer)
+            if (eMessage == eMessageFor) {
+                messagesBuffer.remove(eMessageFor);
+                return true;
+            }
+        return false;
+    }
+
+    public static ArrayList<EMessages> getMessages() {
+        return messagesBuffer;
+    }
+
+    public static boolean removeMessage(EMessages eMessage) {
+        return messagesBuffer.remove(eMessage);
+    }
 
 }
