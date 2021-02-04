d853e93933715d64a7cd8b0d2860e685e595c47d
TheTirea <30987354+TheTirea@users.noreply.github.com>
2021-02-04 10:47:53 +0100

    Basic Window Creation

--- src/main/java/com/tireadev/shadowengine/Launch.java
+++ src/main/java/com/tireadev/shadowengine/Launch.java
@@ -2,6 +2,7 @@ package com.tireadev.shadowengine;
 
 public class Launch {
     public static void main(String[] args) {
-
+        Window.createWindow(256*2,  240*2, "demo");
+        while (true);
     }
 }
