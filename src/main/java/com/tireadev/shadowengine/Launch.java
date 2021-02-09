2427f843c27e3994f23caac039c1dc889e3dd270
TheTirea <30987354+TheTirea@users.noreply.github.com>
2021-02-09 08:52:02 +0100

    Main Inheritable class

--- src/main/java/com/tireadev/shadowengine/Launch.java
+++
@@ -1,8 +0,0 @@
-package com.tireadev.shadowengine;
-
-public class Launch {
-    public static void main(String[] args) {
-        Window.createWindow(256*2,  240*2, "demo");
-        while (true);
-    }
-}
